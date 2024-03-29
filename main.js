20737a657d6a34cc3a1c5486ed6390408cbe89c3
Gustavo Chen <gustavo.sao@gmail.com>
2024-03-29 01:51:38 -0300

    Make intersection work

--- main.js
+++ main.js
@@ -1,23 +1,18 @@
 import "./style.css";
-import viteLogo from "/vite.svg";
-import { setupCounter } from "./counter.js";
 
-document.querySelector("#app").innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`;
+let descriptionBox = document.querySelector(".description-box");
 
-setupCounter(document.querySelector("#counter"));
+let observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.intersectionRatio < 1) {
+        descriptionBox.classList.add("fixed");
+      } else {
+        descriptionBox.classList.remove("fixed");
+      }
+    });
+  },
+  { threshold: 1 },
+);
+
+observer.observe(descriptionBox);
