ffba40267604726781c25c2ca8b36030f22e2b11
Gustavo Chen <gustavo.sao@gmail.com>
2024-04-06 16:17:15 -0300

    Checkpoint

--- main.js
+++ main.js
@@ -1,4 +1,17 @@
-import "./style.css";
+const descriptionBoxRegularInnerHTML = `
+  <div class="name-box">
+    <h3>Gabriela Brás</h3>
+    <p class="divider"></p>
+    <h3>ximbica</h3>
+  </div>
+  <div class="content">
+    <h4>Estudante de moda na universidade de São Paulo, <b>maquiadora profissional</b>.</h4>
+  </div>
+`;
+
+const descriptionBoxFixedInnerHTML = `
+  <div>laskdjfldas</div>
+`;
 
 let descriptionBox = document.querySelector(".description-box");
 
@@ -6,8 +19,10 @@ let observer = new IntersectionObserver(
   (entries) => {
     entries.forEach((entry) => {
       if (entry.intersectionRatio < 1) {
+        descriptionBox.innerHTML = descriptionBoxFixedInnerHTML;
         descriptionBox.classList.add("fixed");
       } else {
+        descriptionBox.innerHTML = descriptionBoxRegularInnerHTML;
         descriptionBox.classList.remove("fixed");
       }
     });
