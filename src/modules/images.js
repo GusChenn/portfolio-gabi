598bbc064f240b24d53417274fcac5afa4edbe47
Gustavo Chen <gustavo.sao@gmail.com>
2024-04-07 19:38:31 -0300

    Change images order and polish up styles

--- src/modules/images.js
+++ src/modules/images.js
@@ -4,7 +4,7 @@ const gallery = document.getElementById("gallery");
 const makeGalleryBox = (imagePath) => {
   const galleryEntry = `
     <div class="gallery-box">
-      <img src="${imagePath}" alt="gallery image" />
+      <img class="gallery-box-img" src="${imagePath}" alt="foto de maquiagem feita pela Gabriela" />
     </div>
   `;
 
@@ -14,3 +14,15 @@ const makeGalleryBox = (imagePath) => {
 for (const imagePath in images) {
   makeGalleryBox(imagePath);
 }
+
+const galleryBoxImages = document.querySelectorAll(".gallery-box-img");
+
+galleryBoxImages.forEach((galleryBoxImage) => {
+  galleryBoxImage.addEventListener("click", () => {
+    const galleryBoxImageHeight = galleryBoxImage.style.height;
+
+    galleryBoxImageHeight === "800px"
+      ? (galleryBoxImage.style.height = "500px")
+      : (galleryBoxImage.style.height = "800px");
+  });
+});
